b330b8c7a68b83fafe3bcf4efbf9e44ee5d9682b
Allan da Silva <allanweb019@gmail.com>
2015-01-30 00:38:53 -0200

    teste
    commit

---
+++ www/js/app.js
@@ -0,0 +1,16 @@
+// $app = angular.module('app',['ngRoute', 'ngAnimate','ngSanitize']);
+
+// //----- Função executada apenas na abertura do aplicativo
+// $app.run(function($rootScope,$sce) {
+// });
+// //----- Configuração de roteamento
+// $app.config(function($routeProvider,$httpProvider){
+    
+//     $routeProvider
+//         .when('/:id',{templateUrl:'index.html'})
+//         .when('/:acao/:id',{templateUrl:'index.html'})
+       
+
+        
+    
+// })
