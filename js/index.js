869212f85b65564dad46a4a031f9f915ae637b49
Allan da Silva <allan@digitalone.com.br>
2016-03-31 10:43:06 -0300

    Atualização do Curd
    Foi atualizado o crud com delete e com editar

--- js/index.js
+++ js/index.js
@@ -3,37 +3,38 @@
                 db.transaction(function(tx) {
                       tx.executeSql(sql,[],onSuccess,onError);
                       function onSuccess(tx,rs){
-                            console.log(rs)
+                            // console.log(rs)
                             }
 
                             function onError(tx,msg){
-                            console.log(msg)
+                            // console.log(msg)
                             }
 
                 });
                 $app = angular.module('app', ['ngRoute', 'ngAnimate','ngSanitize']);
                 $app.run(function($rootScope,$sce) {
                 });
-    //----- Configuração de roteamento
-        $app.config(['$routeProvider','$httpProvider', function($routeProvider,$httpProvider){
-            
-            $routeProvider
-                // .when('/:gid' ,{controller:'ListaController', templateUrl:'index.html'})
-                // .when('/:acao/:id',{templateUrl:'index.html'})
-                
-                 .when('<xxx></xxx>/:xxx', {
-                    controller: "ListaController",
-                    templateUrl: "index.html"
-                })
-                 .otherwise({ redirectTo: '/' })
-              
-            }]);
+            //----- Configuração de roteamento
+                $app.config(['$routeProvider','$httpProvider', function($routeProvider,$httpProvider){
+                    
+                    $routeProvider
+                        .when('/:gid' ,{controller:'ListaController', templateUrl:'index.html'})
+                        .when('/:acao/:id',{templateUrl:'index.html'})
+                        
+                         .when('<xxx></xxx>/:xxx', {
+                            controller: "ListaController",
+                            templateUrl: "index.html"
+                        })
+                         .otherwise({ redirectTo: '/' })
+                      
+                    }]);
 
                 
                 $app.controller('ListaController', function($scope, $routeParams,$rootScope){
                     var id = $routeParams.xxx;
-                    console.log(id);
-
+                    // console.log(id);
+                    // console.log("dsd",$routeParams);
+                    // console.log("dsd",$rootScope);
                     $scope.lista = {};
                     $scope.Vtotal = function(){
                         $scope.lista.total = $scope.lista.valor * $scope.lista.qtd;
@@ -47,16 +48,21 @@
                         }
 
                     }
+
                     $scope.salvaItem = function(){
+                    var Iid =  $scope.lista.iditem; 
+                    console.log(Iid)
                     var Inome =  $scope.lista.nome; 
                     var Ivalor = $scope.lista.valor;
                     var Iqtd = $scope.lista.qtd;
                     var Istatus = 'a';
                     var Itotal = Ivalor * Iqtd;
                     
-                       
-
-                        var sql = "INSERT INTO itens ('iditem', 'nome', 'valor', 'qtd', 'total','status') VALUES (NULL,'"+Inome+"','"+Ivalor+"','"+Iqtd+"','"+Itotal+"','"+Istatus+"')";
+                        if(Iid == undefined ){
+                            var sql = "INSERT INTO itens ('iditem', 'nome', 'valor', 'qtd', 'total','status') VALUES (NULL,'"+Inome+"','"+Ivalor+"','"+Iqtd+"','"+Itotal+"','"+Istatus+"')";
+                        }else{
+                             var sql = "UPDATE itens SET nome='"+Inome+"', valor='"+Ivalor+"', qtd='"+Iqtd+"', total='"+Itotal+"' WHERE iditem ='"+Iid+"'"; 
+                        }
 
                         db.transaction(function(tx) {
                         tx.executeSql(sql,[],onSuccess,onError);
@@ -74,7 +80,36 @@
                          $scope.reset();
 
                     }
+
+                    $scope.delete = function(id){
+                        
+                        var sql = "DELETE FROM itens WHERE iditem="+id;
+
+                        db.transaction(function(tx) {
+                        tx.executeSql(sql,[],onSuccess,onError);
+
+                        function onSuccess(tx,rs){
+                        console.log(rs)
+                        $scope.atulizaLista();
+                         
+                        }
+
+                        function onError(tx,msg){
+                        console.log(msg)
+                        }
+                        });
+                         $scope.reset();
+                    }
+                    $scope.edit = function(produto){
+
+                        $scope.lista = produto;
+                        $scope.atulizaLista();
+
+                    }
+
+
                      $scope.reset = function() {
+                            $scope.lista.iditem = angular.copy($scope.master);
                             $scope.lista.nome = angular.copy($scope.master);
                             $scope.lista.valor = angular.copy($scope.master);
                             $scope.lista.qtd = angular.copy($scope.master);
@@ -94,7 +129,7 @@
                         var result = arrayDb(rs.rows);
                         $scope.$apply(function(){
                                 $scope.lista.item = result;
-                                console.log($scope.lista.item) //-result é um array igual ao da consulta, mas pode ser alterado.
+                                console.log('res',$scope.lista.item) //-result é um array igual ao da consulta, mas pode ser alterado.
                             
                         })
                         }
@@ -111,6 +146,25 @@
                         console.log(msg)
                         }
                     });
+
+                        
+
+
+                        function getMoney( str )
+                        {
+                                return parseInt( str.replace(/[\D]+/g,'') );
+                        }
+                        function formatReal( int )
+                        {
+                                var tmp = int+'';
+                                tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
+                                if( tmp.length > 6 )
+                                        tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+
+                                return tmp;
+                        }
+
+
 
 
                     arrayDb = function(a){
